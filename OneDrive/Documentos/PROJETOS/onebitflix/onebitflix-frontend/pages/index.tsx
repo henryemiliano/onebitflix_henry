21e3c663815a8cef149f317e066f59180fcac81e
Henry <henry.emiliano@hotmail.com>
2023-12-02 15:53:50 -0300

    Configuração incial do projeto, configurando arquivos e pastas. Instalação do sass e do bootstrap.

---
+++ OneDrive/Documentos/PROJETOS/onebitflix/onebitflix-frontend/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import styles from "../styles/HomeNotAuth.module.scss";
+
+const HomeNotAuth = () => {
+  return (
+    <>
+      <Head>-</Head>
+      <main></main>
+    </>
+  );
+};
+
+export default HomeNotAuth;
