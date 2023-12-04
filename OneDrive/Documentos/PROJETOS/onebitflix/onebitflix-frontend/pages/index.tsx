59902fcdc7a80634d355dc79b6fbd0d075a74340
Henry <henry.emiliano@hotmail.com>
2023-12-04 15:42:14 -0300

    Criação dos elementos, do visual e também da sua responsividade - Seção de apresentação, homeNoAuth

--- OneDrive/Documentos/PROJETOS/onebitflix/onebitflix-frontend/pages/index.tsx
+++ OneDrive/Documentos/PROJETOS/onebitflix/onebitflix-frontend/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import HeaderNoAuth from "../src/components/homeNoAuth/headerNoAuth/index";
+import PresentationSection from "@/src/components/homeNoAuth/presentationSection";
+import styles from "@/styles/HomeNotAuth.module.scss";
 
 const HomeNotAuth = () => {
   return (
@@ -14,7 +16,10 @@ const HomeNotAuth = () => {
         />
       </Head>
       <main>
-        <HeaderNoAuth />
+        <div className={styles.sectionBackground}>
+          <HeaderNoAuth />
+          <PresentationSection />
+        </div>
       </main>
     </>
   );
