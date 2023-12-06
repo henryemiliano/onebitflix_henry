5282155c4dfe5096b1a34b9b944ae4d6f7375f08
Henry <henry.emiliano@hotmail.com>
2023-12-06 14:43:20 -0300

    Criando a conexão do backend para login

--- OneDrive/Documentos/PROJETOS/onebitflix/onebitflix-frontend/pages/register.tsx
+++ OneDrive/Documentos/PROJETOS/onebitflix/onebitflix-frontend/pages/register.tsx
@@ -1,19 +1,201 @@
+/* eslint-disable @next/next/no-sync-scripts */
 import HeaderGeneric from "@/src/components/common/headerGeneric";
 import styles from "../styles/registerLogin.module.scss";
 import Head from "next/head";
+import { Form, FormGroup, Label, Container, Button, Input } from "reactstrap";
+import Footer from "@/src/components/common/footer";
+import { FormEvent, useState } from "react";
+import authService from "@/src/services/authService";
+import { useRouter } from "next/router";
+import ToastComponent from "@/src/components/common/toast";
 
 const Register = function () {
+  const router = useRouter();
+
+  const [toastIsOpen, setToastIsOpen] = useState(false);
+  const [toastMessage, setToastMessage] = useState("");
+
+  const handleRegister = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const firstName = formData.get("firstName")!.toString();
+    const lastName = formData.get("lastName")!.toString();
+    const phone = formData.get("phone")!.toString();
+    const birth = formData.get("birth")!.toString();
+    const email = formData.get("email")!.toString();
+    const password = formData.get("password")!.toString();
+    const confirmPassword = formData.get("confirmPassword")!.toString();
+    const params = {
+      firstName,
+      lastName,
+      phone,
+      birth,
+      email,
+      password,
+    };
+
+    if (password != confirmPassword) {
+      setToastIsOpen(true);
+      setTimeout(() => {
+        setToastIsOpen(false);
+      }, 1000 * 3);
+      setToastMessage("As senhas não correspondem.");
+
+      return;
+    }
+
+    const { data, status } = await authService.register(params);
+
+    if (status === 201) {
+      router.push("/login?registred=true");
+    } else {
+      setToastIsOpen(true);
+      setTimeout(() => {
+        setToastIsOpen(false);
+      }, 1000 * 3);
+      setToastMessage(data.message);
+
+      return;
+    }
+
+    if (password.length && confirmPassword.length <= 5) {
+      setToastIsOpen(true);
+      setTimeout(() => {
+        setToastIsOpen(false);
+      }, 1000 * 3);
+      setToastMessage("A senha precisa conter 6 ou mais caracteres.");
+    }
+  };
+
   return (
     <>
       <Head>
         <title>Onebitflix - Registro</title>
         <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+        <script src="https://jsuites.net/v4/jsuites.js"></script>
       </Head>
-      <main>
+      <main className={styles.main}>
         <HeaderGeneric
           logoUrl="/"
           btnContent="Quero fazer login"
           btnUrl="/login"
+        />
+        <Container className="py-5">
+          <p className={styles.formTitle}>Bem-vindo ao OneBitFlix!</p>
+          <Form className={styles.form} onSubmit={handleRegister}>
+            <p className="text-center">
+              <strong>Faça seu cadastro</strong>
+            </p>
+            <FormGroup>
+              <Label for="firstName" className={styles.label}>
+                NOME
+              </Label>
+              <Input
+                id="firstName"
+                name="firstName"
+                type="text"
+                placeholder="Seu nome"
+                required
+                maxLength={20}
+                className={styles.inputName}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="lastName" className={styles.label}>
+                SOBRENOME
+              </Label>
+              <Input
+                id="lastName"
+                name="lastName"
+                type="text"
+                placeholder="Seu sobrenome"
+                required
+                maxLength={20}
+                className={styles.inputName}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="phone" className={styles.label}>
+                TELEFONE
+              </Label>
+              <Input
+                id="phone"
+                name="phone"
+                type="tel"
+                placeholder="(xx) xxxxx-xxxx"
+                data-mask="[-]+55 (00) 00000-0000"
+                required
+                className={styles.input}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="email" className={styles.label}>
+                EMAIL
+              </Label>
+              <Input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="Seu melhor email"
+                required
+                className={styles.input}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="birth" className={styles.label}>
+                DATA DE NASCIMENTO
+              </Label>
+              <Input
+                id="birth"
+                name="birth"
+                type="date"
+                min="1930-01-01"
+                max="2020-12-31"
+                required
+                className={styles.input}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="password" className={styles.label}>
+                SENHA
+              </Label>
+              <Input
+                id="password"
+                name="password"
+                type="password"
+                placeholder="Digite a sua senha (Min: 6 | Max: 20)"
+                required
+                minLength={6}
+                maxLength={20}
+                className={styles.input}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="confirmPassword" className={styles.label}>
+                CONFIRME SUA SENHA
+              </Label>
+              <Input
+                id="confirmPassword"
+                name="confirmPassword"
+                type="password"
+                placeholder="Confirme a sua senha"
+                required
+                minLength={6}
+                maxLength={20}
+                className={styles.input}
+              />
+            </FormGroup>
+            <Button type="submit" outline className={styles.formBtn}>
+              CADASTRAR
+            </Button>
+          </Form>
+        </Container>
+        <Footer />
+        <ToastComponent
+          color="bg-danger"
+          isOpen={toastIsOpen}
+          message={toastMessage}
         />
       </main>
     </>
